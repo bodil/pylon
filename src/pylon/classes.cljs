d29a0651c6e7e8e05464cfed77f85dff552fab48
Bodil Stokke <bodil@bodil.tv>
2013-01-01 02:50:57 +0100

    Now 100% (js*) free!

--- src/pylon/classes.cljs
+++ src/pylon/classes.cljs
@@ -1,45 +1,49 @@
 (ns pylon.classes)
 
+(defn- method-fn-name
+  [method-name]
+  (str "__pylon$method$" method-name))
+
 (defn create-ctor []
-  (js* "function ctor() {
-  var obj = this, p = Object.getPrototypeOf(this),
-      superclass = p.__pylon$superclass;
-  if (this.__pylon$bind) {
-    this.__pylon$bind.forEach(function(f) {
-      obj[f] = goog.bind(obj[f], obj);
-    });
-  }
-  this.__pylon_invokeSuper = function(name) {
-    var args = goog.array.toArray(arguments).slice(1);
-    if (superclass.__pylon$classname) {
-      name = \"__pylon$method$\" + name;
-      args = goog.array.concat(obj, args);
-    }
-    if (name == \"constructor\" && !superclass.prototype[name])
-      return superclass.apply(obj, args);
-    else
-      return superclass.prototype[name].apply(obj, args);
-  };
-  if (this.hasOwnProperty(\"constructor\"))
-    this.constructor.apply(this, arguments);
-}"))
+  (fn ctor [& args]
+    (this-as
+     this
+     (let [p (js/Object.getPrototypeOf this)
+           superclass (aget p "__pylon$superclass")]
+       (when-let [binds (aget p "__pylon$bind")]
+         (doseq [bind binds]
+           (aset this bind (goog/bind (aget this bind) this))))
+       (aset this "__pylon_invokeSuper"
+             (fn [name & args]
+               (let [name (if (aget superclass "__pylon$classname")
+                            (method-fn-name name) name)
+                     args (if (aget superclass "__pylon$classname")
+                            (cons this args) args)]
+                 (let [super-method (aget (.-prototype superclass) name)
+                       super-fn (if (and (= name "constructor")
+                                         (not super-method))
+                               superclass super-method)
+                       args (into-array args)]
+                   (.apply super-fn this args))))))
+     (when (.hasOwnProperty this "constructor")
+       (apply (.-constructor this) args))
+     this)))
 
 (defn apply-method [ctor func methodname funcname]
-  (js* "(function(ctor, name, fn_name, fn) {
-  var p = ctor.prototype;
-  p[fn_name] = fn;
-  p[name] = function() {
-    var args = goog.array.concat(this, goog.array.toArray(arguments));
-    return this[fn_name].apply(this, args);
-  };
-  if (!p.__pylon$bind) p.__pylon$bind = [];
-  p.__pylon$bind.push(name);
-})(~{ctor}, ~{methodname}, ~{funcname}, ~{func})"))
+  (let [p (.-prototype ctor)]
+    (aset p funcname func)
+    (aset p methodname
+          (fn [& args]
+            (this-as this (apply (aget this funcname) (cons this args)))))
+    (when-not (aget p "__pylon$bind")
+      (aset p "__pylon$bind" (array)))
+    (.push (aget p "__pylon$bind") methodname)))
 
 (defn define-superclass [ctor superclass]
   (when superclass
     (goog/inherits ctor superclass)
-    (js* "~{ctor}.prototype.__pylon$superclass = ~{superclass}")))
+    (let [p (.-prototype ctor)]
+      (aset p "__pylon$superclass" superclass))))
 
 (defn apply-mixins [ctor mixins]
   (when (seq mixins)
