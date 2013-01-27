3a4d228655c15ac2041805a18ca8e3f35a953191
Bodil Stokke <bodil@bodil.tv>
2013-01-27 17:00:04 +0100

    Bump for 0.3.0 release.

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject org.bodil/pylon "0.2.0"
+(defproject org.bodil/pylon "0.3.0"
   :description "A Javascript class system in 100% Clojurescript"
   :url "https://github.com/bodil/pylon"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :plugins [[lein-cljsbuild "0.2.9"]
-            [org.bodil/lein-noderepl "0.1.1"]]
+  :plugins [[lein-cljsbuild "0.2.10"]
+            [org.bodil/lein-noderepl "0.1.4"]]
   :cljsbuild {:builds
               [{:source-path "src"
                 :compiler
                 {:output-to "js/main.js"
                  :output-dir "js"
-                 :optimizations :advanced
+                 :optimizations :simple
                  :target :nodejs
                  :jar true}}]}
   :main "js/main.js")
